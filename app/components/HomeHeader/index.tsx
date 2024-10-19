6e348091c5781f90f93eacc547720919b5fdfe31
Daniel Bui <79790753+danielbui12@users.noreply.github.com>
2024-10-19 19:17:09 +0700

    update logo

--- app/components/HomeHeader/index.tsx
+++ app/components/HomeHeader/index.tsx
@@ -1,8 +1,8 @@
 import { Button, Navbar } from "flowbite-react";
-import { Logo } from "../Icon";
 import { useAppKit } from "@reown/appkit/react";
 import { useAccount } from "wagmi";
 import { shortenWalletAddress } from "@/app/utils/helper";
+import Image from "next/image";
 
 export const HomeHeader = () => {
     const { open } = useAppKit()
@@ -10,7 +10,12 @@ export const HomeHeader = () => {
     return (
         <Navbar fluid rounded>
             <Navbar.Brand href={process.env.NEXT_PUBLIC_SITE_URL!}>
-                <Logo />
+                <Image
+                    src={"/logo.jpeg"}
+                    width={65}
+                    height={65}
+                    alt="Bookadot"
+                />
             </Navbar.Brand>
             <div>
                 <Button
--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,28 @@ export default function RootLayout({
             <head>
                 <ThemeModeScript />
                 <title>Bookadot</title>
+                {/* Standard Favicon */}
+                <link rel="icon" type="image/jpeg" href="/logo.jpeg" />
+
+                {/* For iOS */}
+                <link rel="apple-touch-icon" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="120x120" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/logo.jpeg" />
+
+                {/* Android */}
+                <link rel="icon" sizes="192x192" href="/logo.jpeg" />
+                <link rel="icon" sizes="256x256" href="/logo.jpeg" />
+                <link rel="icon" sizes="384x384" href="/logo.jpeg" />
+                <link rel="icon" sizes="512x512" href="/logo.jpeg" />
+
+                {/* For Windows (pinned site) */}
+                <meta name="msapplication-TileImage" content="/logo.jpeg" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+
+                {/* General meta for theming */}
+                <meta name="theme-color" content="#ffffff" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0" />
             </head>
             <body className={"bookadot-body flex w-full flex-col items-center"}>
